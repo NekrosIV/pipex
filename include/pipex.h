7a447c40f8f2673e2e1a65ddc81cbd2958a1d81c
NekrosIV <singh.karandeep2106@gmail.com>
2024-02-03 10:17:59 +0100

    add include fils

---
+++ include/pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kasingh <kasingh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 13:17:24 by kasingh           #+#    #+#             */
+/*   Updated: 2024/01/29 13:20:37 by kasingh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <limits.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
