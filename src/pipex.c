5e8f4ae4458361ccddcf925147c5e1903f40617e
NekrosIV <singh.karandeep2106@gmail.com>
2024-02-16 13:09:39 +0100

    pipex works and handle all error

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: kasingh <kasingh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:58:33 by kasingh           #+#    #+#             */
-/*   Updated: 2024/02/09 12:20:36 by kasingh          ###   ########.fr       */
+/*   Updated: 2024/02/16 12:11:45 by kasingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,6 @@ char	*get_path(char **cmd, char **env, char **path)
 	char	*tmp;
 	char	*slash_cmd;
 
-	if (access(cmd[0], F_OK) == 0)
-		return (ft_strdup(cmd[0]));
 	good_path = NULL;
 	slash_cmd = ft_strjoin("/", cmd[0]);
 	if (!slash_cmd)
@@ -53,12 +51,25 @@ char	*get_path(char **cmd, char **env, char **path)
 	free_split(path);
 	return (good_path);
 }
+
 void	error_msg(char *path, char **cmd)
 {
-	ft_putstr_fd("command not found: ", 2);
+	if (!path && ft_strchr(cmd[0], '/') != NULL)
+		ft_putstr_fd("No such file or directory : ", 2);
+	else if (!path)
+		ft_putstr_fd("command not found: ", 2);
+	else if (access(path, F_OK) == 0 && access(path, X_OK) != 0)
+	{
+		ft_putstr_fd("Permission denied: ", 2);
+		ft_putendl_fd(cmd[0], 2);
+		free_split(cmd);
+		free(path);
+		exit(126);
+	}
+	if (path)
+		free(path);
 	ft_putendl_fd(cmd[0], 2);
 	free_split(cmd);
-	free(path);
 	exit(127);
 }
 
@@ -68,31 +79,38 @@ void	excute(char **cmd, char **env)
 	char	*path;
 	char	**tmp_path;
 
+	tmp_path = NULL;
+	path = NULL;
 	i = 0;
-	while (ft_strncmp(env[i], "PATH=", 5) != 0 && env[i])
-		i++;
-	if (env[i])
-		tmp_path = ft_split(&env[i][5], ':');
-	if (!tmp_path)
+	if (access(cmd[0], F_OK) == 0)
+		path = ft_strdup(cmd[0]);
+	else if (env[i])
 	{
-		perror("Error getting path");
-		exit(EXIT_FAILURE);
+		while (env[i] && ft_strncmp(env[i], "PATH=", 5) != 0)
+			i++;
+		if (env[i])
+			tmp_path = ft_split(&env[i][5], ':');
+		if (!tmp_path)
+			error_msg(path, cmd);
+		path = get_path(cmd, env, tmp_path);
 	}
-	path = get_path(cmd, env, tmp_path);
 	if (!path)
 		error_msg(path, cmd);
 	execve(path, cmd, env);
 	error_msg(path, cmd);
 }
+
 char	**get_cmd(char *cmd)
 {
 	char	**cmd_split;
 
 	cmd_split = ft_split(cmd, ' ');
-	if (!cmd_split)
+	if (!cmd_split || !cmd_split[0])
 	{
-		perror("Error getting command");
-		exit(EXIT_FAILURE);
+		ft_putstr_fd("Command not found: ", 2);
+		ft_putendl_fd(cmd, 2);
+		free(cmd_split);
+		exit(127);
 	}
 	return (cmd_split);
 }
@@ -105,6 +123,7 @@ void	child(int pipe_fd[2], char **av, char **env)
 	fd_in = open(av[1], O_RDONLY);
 	if (fd_in == -1)
 	{
+		close(pipe_fd[1]);
 		perror(av[1]);
 		exit(1);
 	}
@@ -131,6 +150,7 @@ void	child2(int pipe_fd[2], char **av, char **env)
 	fd_out = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_out == -1)
 	{
+		close(pipe_fd[0]);
 		perror(av[4]);
 		exit(EXIT_FAILURE);
 	}
@@ -149,12 +169,30 @@ void	child2(int pipe_fd[2], char **av, char **env)
 	excute(get_cmd(av[3]), env);
 }
 
+int	wait_for_child(pid_t pid)
+{
+	int		status[2];
+	pid_t	r_waitpid;
+
+	while (1)
+	{
+		r_waitpid = waitpid(-1, &status[0], 0);
+		if (r_waitpid == -1)
+			break ;
+		if (r_waitpid == pid)
+			status[1] = status[0];
+	}
+	if (WIFEXITED(status[1]))
+		return (WEXITSTATUS(status[1]));
+	else if (WIFSIGNALED(status[1]))
+		return (WTERMSIG(status[1]) + 128);
+	return (0);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	int		pipe_fd[2];
 	pid_t	pid;
-	pid_t	r_waitpid;
-	int		status[2];
 
 	if (ac != 5 || pipe(pipe_fd) == -1)
 		return (1);
@@ -170,10 +208,5 @@ int	main(int ac, char **av, char **env)
 		child2(pipe_fd, av, env);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
-	while ((r_waitpid = waitpid(-1, &status[0], 0)) > 0)
-		if (r_waitpid == pid)
-			status[1] = status[0];
-	if (WIFEXITED(status[1]))
-		return (WEXITSTATUS(status[1]));
-	return (0);
+	return (wait_for_child(pid));
 }
